de1c083e1637ac105f5cea5b6c8cce88e0af595c
Remy Ho <remyho427@gmail.com>
2022-01-24 22:15:38 +0800

    removed preact-router for time-being

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,8 @@ import '@fontsource/roboto/700.css';
 import './styles/reset.css';
 import './styles/global.css';
 
-import Router from 'preact-router';
 import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contacts from './pages/Contacts';
 
-const App = () => (
-  <Router>
-    <Home path="/" />
-    <About path="/about" />
-    <Projects path="/projects" />
-    <Contacts path="/contact" />
-  </Router>
-);
+const App = () => <Home path="/" />;
 
 export default App;
